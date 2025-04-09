9980a9dbb357c548b8152eb87d06b9e1d41df218
AngelDeGabriel <angeldgabrielaparicio@gmail.com>
2025-04-09 08:08:13 -0600

    prueba

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -116,12 +116,40 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('inicio')">
+                INICIO
             </x-responsive-nav-link>
 
             <x-responsive-nav-link :href="route('productos')" :active="request()->routeIs('productos')">
                 PRODUCTOS
+            </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('productos')" :active="request()->routeIs('equipo')">
+                EQUIPO
+            </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('productos')" :active="request()->routeIs('nosotros')">
+                NOSOTROS
+            </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('productos')" :active="request()->routeIs('morecarac')">
+                TECNOLOGÍA
+            </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('productos')" :active="request()->routeIs('app')">
+                APP
+            </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('productos')" :active="request()->routeIs('posts')">
+                BLOG
+            </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('productos')" :active="request()->routeIs('polites')">
+                PRIVACIDAD
+            </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('productos')" :active="request()->routeIs('Faqs')">
+                FAQ'S
             </x-responsive-nav-link>
         </div>
 
